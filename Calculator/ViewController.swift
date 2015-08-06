ea963b6f73e9fab098827f0fa3ec5543587bb6d7
Julian <julian_veling@hotmail.com>
2015-08-06 22:38:26 +0100

    nearly done

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,17 +9,57 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    @IBOutlet weak var display: UILabel!
+    var userIsTyping = false
+    
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsTyping{
+            display.text = display.text! + digit
+        }else{
+            display.text = digit
+            userIsTyping = true
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    var operandStack = Array<Double>()
+    @IBAction func enter() {
+        userIsTyping = false
+        operandStack.append(displayValue)
+        println("\(operandStack)")
     }
-
-
+    
+    var displayValue: Double {
+        get{
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set{
+            display.text = "\(newValue)"
+            userIsTyping = false
+        }
+    }
+    
+    @IBAction func operate(sender: UIButton) {
+        let operation = sender.currentTitle!
+        if userIsTyping{
+            enter()
+        }
+        switch operation{
+        case "x": doCalc {$0 * $1}
+        case "÷": doCalc {$0 / $1}
+        case "-": doCalc {$0 - $1}
+        case "+": doCalc {$0 + $1}
+        default: break
+        }
+    }
+    
+    func doCalc(operation: (Double, Double) -> Double){
+        if operandStack.count >= 2{
+            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+            enter()
+        }
+    }
+    
 }
 
